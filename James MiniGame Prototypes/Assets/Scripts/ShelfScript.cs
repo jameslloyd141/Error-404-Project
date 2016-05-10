c722600e21792a010ffe701086f33d7d5fc2bb9d
James Lloyd <jameslloyd141@gmail.com>
2016-05-10 12:23:31 +0100

    Added more to code

---
+++ James MiniGame Prototypes/Assets/Scripts/ShelfScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShelfScript : MonoBehaviour
+{
+    //public GameObject player;
+    public MoveScript MoveScript1;
+    public int shelfNumber;
+
+
+    // Use this for initialization
+    /*void start()
+    {
+        //player = GameObject.Find("player");
+        MoveScript1 = player.GetComponent<MoveScript>();
+    }*/
+    void OnMouseDown()
+    {
+        MoveScript1.CurrentShelf = shelfNumber;
+    }
+}
