5f6c244224fdb93984bd836df15f5757d340d6a1
James Lloyd <jameslloyd141@gmail.com>
2016-05-10 11:23:40 +0100

    ads

--- James MiniGame Prototypes/Assets/Scripts/MoveScript.cs
+++ James MiniGame Prototypes/Assets/Scripts/MoveScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MoveScript : MonoBehaviour {
     //public Transform goal;
     NavMeshAgent agent;
+    public int CurrentShelf = 0;
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -12,10 +13,11 @@ public class MoveScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Debug.Log(CurrentShelf);
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-
+            CurrentShelf = 0;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
                 agent.destination = hit.point;
