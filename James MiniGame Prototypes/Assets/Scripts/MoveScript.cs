d3efd746a16fea3b97d36ad34d444e8e2d6c249d
James Lloyd <jameslloyd141@gmail.com>
2016-05-10 10:19:30 +0100

    Move script works

--- James MiniGame Prototypes/Assets/Scripts/MoveScript.cs
+++ James MiniGame Prototypes/Assets/Scripts/MoveScript.cs
@@ -2,15 +2,27 @@
 using System.Collections;
 
 public class MoveScript : MonoBehaviour {
-    public Transform goal;
+    //public Transform goal;
+    NavMeshAgent agent;
     // Use this for initialization
     void Start () {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = goal.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+
+        }
+    }
 }
+
+//http://docs.unity3d.com/Manual/nav-MoveToClickPoint.html
